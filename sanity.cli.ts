13396f4c167f80d89f7379d7a7bf412ae3aa7dcb
KarthikC-777 <karthik.c@codeandtheory.com>
2025-11-30 17:39:03 +0530

    fix: fixed sanity cli.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,7 +3,8 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '9htg8iyy',
-    dataset: 'dev'
+    dataset: 'dev',
+
   },
   deployment: {
     /**
@@ -11,5 +12,10 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
     autoUpdates: true,
+    appId:"pykfruo7knhoz2pfnpv6824i"
+  },
+  app:{
+    id:"pykfruo7knhoz2pfnpv6824i",
+ organizationId: 'oI3Nv0w6K',
   }
 })
