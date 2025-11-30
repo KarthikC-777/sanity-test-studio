935d593d6d3178cde19270c46c6820ebe76f9a7e
KarthikC-777 <karthik.c@codeandtheory.com>
2025-11-30 17:57:17 +0530

    fix: fixed minor changes.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,15 +6,5 @@ export default defineCliConfig({
     dataset: 'dev',
 
   },
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/cli#auto-updates
-     */
-    autoUpdates: true,
-  },
-  app:{
-    id:"pykfruo7knhoz2pfnpv6824i",
- organizationId: 'oI3Nv0w6K',
-  }
+  
 })
